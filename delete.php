63dc800a5e9933e954a6775db8e2af724d701ede
Renil_Soni_0653 <69391253+RenilSoni0653@users.noreply.github.com>
2020-10-01 14:14:36 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,3 +5,11 @@ $db->connect();
 $db->delete('CRUDClass','id=5');  // Table name, WHERE conditions
 $res = $db->getResult();  
 print_r($res);
+
+//Another way to delete record in PHP
+<?php
+  include('class/mysql_crud.php');
+  $connect = mysqli_connect("localhost","root","","CRUD");
+  $query = mysqli_query('select * from CRUDClass where id = 5');  // Table name, WHERE conditions
+  echo $query;
+?>
